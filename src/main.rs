0e6f72470ec59ec5e98d97337c06ce6f64d13095
Serhii Tokariev <seregat1984@gmail.com>
2022-01-24 15:15:59 +0200

    Filtered input sources

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,112 @@
-use core_foundation::array::{CFArrayGetCount, CFArrayGetValueAtIndex, CFArrayRef};
-use std::ffi::c_void;
+use core_foundation::{
+    array::{CFArrayGetCount, CFArrayGetValueAtIndex, CFArrayRef},
+    base::CFIndexConvertible,
+    boolean::{kCFBooleanTrue, CFBooleanRef},
+    dictionary::{CFDictionaryCreate, CFDictionaryRef},
+    string::kCFStringEncodingUTF8,
+    string::{CFStringGetCStringPtr, CFStringRef},
+};
+use std::ffi::{c_void, CStr};
 
 struct __TISInputSource;
 
 #[link(name = "Carbon", kind = "framework")]
 extern "C" {
 
-    fn TISCreateInputSourceList(props: Option<CFArrayRef>, includeAllInstalled: bool)
-        -> CFArrayRef;
+    fn TISCreateInputSourceList(props: CFDictionaryRef, includeAllInstalled: bool) -> CFArrayRef;
+
+    fn TISGetInputSourceProperty(
+        inputSource: *const c_void,
+        propertyKey: CFStringRef,
+    ) -> *const c_void;
 
     fn TISSelectInputSource(input_source: *const c_void) -> i32;
+
+    static kTISPropertyInputSourceID: CFStringRef;
+    static kTISPropertyInputSourceCategory: CFStringRef;
+    static kTISPropertyInputSourceType: CFStringRef;
+    static kTISPropertyInputSourceIsEnabled: CFStringRef;
+    static kTISPropertyInputSourceIsSelected: CFStringRef;
+
+    static kTISTypeKeyboardLayout: CFStringRef;
 
 }
 
 fn main() {
-    println!("Hello, world!");
-
     unsafe {
-        let list = TISCreateInputSourceList(None, false);
+        let keys: Vec<*const c_void> = vec![
+            std::mem::transmute(kTISPropertyInputSourceIsEnabled),
+            std::mem::transmute(kTISPropertyInputSourceType),
+        ];
 
+        let values: Vec<*const c_void> = vec![
+            std::mem::transmute(kCFBooleanTrue),
+            std::mem::transmute(kTISTypeKeyboardLayout),
+        ];
+
+        let filter = CFDictionaryCreate(
+            std::ptr::null(),
+            keys.as_ptr(),
+            values.as_ptr(),
+            keys.len().to_CFIndex(),
+            std::ptr::null(),
+            std::ptr::null(),
+        );
+
+        println!("Dictionary created");
+
+        println!("Querying languages");
+
+        let list = TISCreateInputSourceList(filter, false);
 
         let cnt = CFArrayGetCount(list);
         println!("Count {}", cnt);
 
-        let lang = CFArrayGetValueAtIndex(list, 0);
+        let cfstr_props = vec![
+            kTISPropertyInputSourceID,
+            kTISPropertyInputSourceCategory,
+            kTISPropertyInputSourceType,
+        ];
+
+        let cfbool_props = vec![
+            kTISPropertyInputSourceIsEnabled,
+            kTISPropertyInputSourceIsSelected,
+        ];
+
+        for i in 0..cnt {
+            let lang = CFArrayGetValueAtIndex(list, i);
+
+            println!("Language #{}", i);
+            for p in &cfstr_props {
+                let prop_void = TISGetInputSourceProperty(lang, *p);
+                let prop: CFStringRef = std::mem::transmute(prop_void);
+
+                println!("{} = {}", from_cf_string_ref(*p), from_cf_string_ref(prop));
+            }
+
+            for p in &cfbool_props {
+                let prop_void = TISGetInputSourceProperty(lang, *p);
+                let prop_bool: CFBooleanRef = std::mem::transmute(prop_void);
+
+                println!(
+                    "{} = {}",
+                    from_cf_string_ref(*p),
+                    prop_bool == kCFBooleanTrue
+                );
+            }
+            println!("\n\n");
+        }
+
+        let lang = CFArrayGetValueAtIndex(list, 1);
         let select_result = TISSelectInputSource(lang);
 
         println!("Result {}", select_result);
     }
+}
+
+unsafe fn from_cf_string_ref(r: CFStringRef) -> String {
+    let c_ptr = CFStringGetCStringPtr(r, kCFStringEncodingUTF8);
+    let prop_val = CStr::from_ptr(c_ptr);
+
+    String::from(prop_val.to_str().unwrap())
 }
